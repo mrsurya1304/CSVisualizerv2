2bd90754c6af67e39f3998d0b8be5354c708a063
mrsurya1304 <mrsurya1304@gmail.com>
2022-09-14 13:11:56 +0530

    first commit

--- src/Home/home.jsx
+++ src/Home/home.jsx
@@ -7,8 +7,8 @@ export default function Home(){ //Homepage
     return (
       <div>
       <h1>CS Visualiser</h1>
-      <p className="intro">Welcome to the CS visualiser. This tool is aimed to promote visual learning and hopefully 
-        give you a deeper insight into the world of algorithms. "A picture is worth a thousand words" 
+      <p className="intro">Welcome to the CS visualiser.<br />This tool is aimed to promote visual learning and hopefully 
+        give you a deeper insight into the world of algorithms.<br /> "A picture is worth a thousand words" 
         with that being said enjoy, learn and play around with our application <br></br>
       </p>
       <button onClick={() => {history.push('/pathfinding')}}>Path finding visualizer</button>
